a5ffa7f5b067eed7c99de3e47b81c34ce3b47ff4
DeePenguin <_deadpenguin_@mail.ru>
2019-10-01 23:56:18 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const parts = expression.split('*');
+  let zeroes = 0;
+  let fives = 0;
+  let isDoubleEven = false
+
+  const powCounter = (number) => {
+    let counter = 0;
+    let denominator = 0;
+    let i = 1;
+    do {
+      denominator = Math.pow(5, i);
+      counter += Math.trunc(number / denominator);
+      i++;
+    } while (number / denominator >= 1);
+    return counter;
+  }
+
+  parts.map(part => {
+    if (part.includes('!!')) {
+      let number = Number(part.slice(0, -2));
+      if (number % 2) {
+        fives += powCounter(number) - powCounter((number - 1) / 2);
+      } else {
+        number /= 2;
+        zeroes += powCounter(number);
+        isDoubleEven = true;
+      }
+
+    } else {
+      let number = Number(part.slice(0, -1));
+      zeroes += powCounter(number);
+    }
+  })
+  zeroes += (zeroes || isDoubleEven) ? fives : 0;
+  return zeroes;
 }
